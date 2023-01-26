b473c381578dad59fd7924a84981cda598175db2
iSicko <74508163+iSickos@users.noreply.github.com>
2023-01-26 14:11:17 +0200

    its gap not space-x space-y

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home({ products }: { products: Product }) {
   return (
     <div>
       <Nav></Nav>
-      <div className=" px-5 pb-5 flex flex-wrap space-x-5 space-y-5 justify-center items-end">
+      <div className=" px-5 pb-5 flex flex-wrap gap justify-center items-end">
         {products.map((product: Product) => {
           return (
             <Card key={product.id} id={product.id} title={product.title} description={product.description} image={product.image} price={product.price} rate={product.rating.rate} count={product.rating.count} />
